c0a85698ac34342345513d9b073ae09c931737b7
anton <antonlukas25@gmail.com>
2023-04-18 01:16:03 +0200

    some Frontend done.
    welcome screen and onetime location done

--- Frontend/Agit/components/welcomeScreens/OneTimeLocation.js
+++ Frontend/Agit/components/welcomeScreens/OneTimeLocation.js
@@ -1,36 +1,107 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, SafeAreaView, StatusBar,TextInput} from 'react-native';
 
 const OneTimeLocation = ({navigation}) => {
+    const [text, onChangeText] = React.useState('');
 
+    const nejTak = () => {
+        console.log(text)
+        navigation.navigate("Login")
+    }
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Engangs lokalition</Text>
-            <Text style={styles.description}>
-                This is the Store screen. You can add your own content here.
-            </Text>
-            <TouchableOpacity onPress={()=>navigation.navigate("Walktrough")}>
-                <Text> done </Text>
-            </TouchableOpacity>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <StatusBar
+                animated={true}
+                barStyle={"default"}
+            />
+            <View style={styles.welcomeContainer}>
+                <View style={styles.titleView}>
+                    <Text style={styles.title}>Engangs Lokalition</Text>
+                </View>
+                <View style={styles.descriptionView}>
+                    <Text style={styles.description}>
+                            Siden du ikke tillader gps lokalition kræver appen en general lokalition for at give dig butikker i dit generale omrøde
+                    </Text>
+                </View>
+
+                <View style={styles.buttonView}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={onChangeText}
+                        value={text}
+                        placeholder="by, postnummer, gadenavn"
+                    />
+                    <TouchableOpacity style={styles.button} onPress={nejTak}>
+                        <Text style={styles.buttonText}> Godkend lokalition </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
+        justifyContent: "center",
+        backgroundColor: "#365054"
+    },
+    welcomeContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        height: "65%",
+        backgroundColor: "#c5dafc",
+        width: "90%",
+        borderRadius: 30,
     },
     title: {
-        fontSize: 24,
+        fontSize: 30,
         fontWeight: 'bold',
-        marginBottom: 16,
+        marginTop: 25,
+    },
+    titleView: {
+        flex: 1,
+        width: "100%",
+        alignItems: "center"
+
     },
     description: {
         fontSize: 18,
         textAlign: 'center',
-        paddingHorizontal: 32,
+        padding: 5,
+        width: "90%",
+    },
+    descriptionView: {
+        flex: 1,
+        width: "100%",
+        alignItems: "center"
+    },
+    button: {
+        width: "80%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+        backgroundColor: "#4bb91b",
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: "600",
+    },
+    buttonView: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 20,
+        flex: 2
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        width: "80%"
     },
 });
 
--- Frontend/Agit/components/welcomeScreens/WelcomeScreen.js
+++ Frontend/Agit/components/welcomeScreens/WelcomeScreen.js
@@ -1,38 +1,102 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, SafeAreaView, StatusBar} from 'react-native';
 
 const WelcomeScreen = ({navigation}) => {
 
+    const tillad = () => {
+        //add location thing
+        navigation.navigate("Login")
+    }
+    const nejTak = () => {
+        navigation.navigate("OneTimeLocation")
+    }
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Welcome to my Agit</Text>
-            <Text style={styles.description}>
-                Denne app kraver internet for at fungere.
+        <SafeAreaView style={styles.container}>
+            <StatusBar
+                animated={true}
+                barStyle={"default"}
+            />
+            <View style={styles.welcomeContainer}>
+                <View style={styles.titleView}>
+                    <Text style={styles.title}>Welcome to Agit</Text>
+                </View>
+                <View style={styles.descriptionView}>
+                    <Text style={styles.description}>
+                        Denne app kraver internet for at fungere.
+                    </Text>
+                    <Text style={styles.description}>
+                        For at få den fulde oplevelse skal Agit bruge din lokation
+                    </Text>
+                </View>
 
-                For at få den fulde oplevelse skal Agit bruge din lokation
-            </Text>
-            <TouchableOpacity onPress={()=> navigation.navigate("OneTimeLocation")}>
-                <Text> No Thank You </Text>
-            </TouchableOpacity>
-        </View>
+                <View style={styles.buttonView}>
+                    <TouchableOpacity style={styles.button} onPress={tillad}>
+                        <Text style={styles.buttonText}> Tillad Lokation</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={nejTak}>
+                        <Text style={styles.buttonText}> Nej Tak </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
+        justifyContent: "center",
+        backgroundColor: "#365054"
+    },
+    welcomeContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        height: "65%",
+        backgroundColor: "#c5dafc",
+        width: "90%",
+        borderRadius: 30,
     },
     title: {
-        fontSize: 24,
+        fontSize: 30,
         fontWeight: 'bold',
-        marginBottom: 16,
+        marginTop: 25,
+    },
+    titleView: {
+        flex: 1,
+        width: "100%",
+        alignItems: "center"
+
     },
     description: {
         fontSize: 18,
         textAlign: 'center',
-        paddingHorizontal: 32,
+        padding: 5,
+        width: "90%",
+    },
+    descriptionView: {
+        flex: 1,
+        width: "100%",
+        alignItems: "center"
+    },
+    button: {
+        width: "80%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+        backgroundColor: "#4bb91b",
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: "600",
+    },
+    buttonView: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 20,
+        flex: 2
     },
 });
 
